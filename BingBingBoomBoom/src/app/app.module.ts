88807f8dc210aeb1d63e02bb07ef48c6f50f0cc0
BrianChen <151617905+brian861024@users.noreply.github.com>
2024-12-26 23:06:52 +0800

    20241226 新增一些基礎元件

--- BingBingBoomBoom/src/app/app.module.ts
+++ BingBingBoomBoom/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layout/default/default.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './memberCenter/register/register.component';
+import { SignInComponent } from './memberCenter/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    RegisterComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
