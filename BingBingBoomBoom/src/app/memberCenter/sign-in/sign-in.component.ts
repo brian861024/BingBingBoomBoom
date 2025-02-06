148601c20663cc29e0bef7cb7226832eeaf7b621
Celine.C <demain1209@gmail.com>
2025-02-07 02:09:51 +0800

    設定帳號密碼輸入條件

--- BingBingBoomBoom/src/app/memberCenter/sign-in/sign-in.component.ts
+++ BingBingBoomBoom/src/app/memberCenter/sign-in/sign-in.component.ts
@@ -3,20 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
 
-  title="會員登入";
-username='';
-password='';
-count=0;
-  confirm(para:any){
+  title = '會員登入';
+  username = '';
+  password = '';
 
-    if(this.username&&this.password===null){
-this.username='666';
-    };
+  confirm(para: MouseEvent) {
 
+    if (!(this.username || this.password)) {
+      this.showError();
+      return;
+    } else  {
+      alert('登入成功!');
+    }
+  }
+
+    noUsername=false;
+    noPassword=false;
+
+showError() {
+    if (!this.username) {
+        this.noUsername = true;
+    } else  {
+        this.noUsername = false;
+    }
+    if (!this.password) {
+      this.noPassword = true;
+  } else  {
+    this.noPassword= false;
+  }
+
+}
 
   }
-}
