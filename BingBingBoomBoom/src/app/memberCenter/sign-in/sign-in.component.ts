340fa9d6ad72e3ebee1e907f3d20d5326ee89dbc
Celine.C <demain1209@gmail.com>
2025-02-13 18:15:55 +0800

    0213

--- BingBingBoomBoom/src/app/memberCenter/sign-in/sign-in.component.ts
+++ BingBingBoomBoom/src/app/memberCenter/sign-in/sign-in.component.ts
@@ -7,35 +7,63 @@ import { Component } from '@angular/core';
 })
 export class SignInComponent {
 
-  title = '會員登入';
-  username = '';
-  password = '';
+//   title = '會員登入';
+//   username = '';
+//   password = '';
 
-  confirm(para: MouseEvent) {
+//   confirm(para: MouseEvent) {
 
-    if (!(this.username || this.password)) {
-      this.showError();
-      return;
-    } else  {
-      alert('登入成功!');
-    }
+//     if (!(this.username || this.password)) {
+//       this.showError();
+//       return;
+//     } else {
+//       alert('登入成功!');
+//     }
+//   }
+
+     noUsername = false;
+     noPassword = false;
+
+// showError() {
+//     if (!this.username) {
+//         this.noUsername = true;
+//     } else  {
+//         this.noUsername = false;
+//     }
+//     if (!this.password) {
+//       this.noPassword = true;
+//   } else  {
+//     this.noPassword= false;
+//   }
+
+// }
+title = '會員登入';
+
+user = {
+  account: "",
+  password: ""
+};
+
+
+
+confirm(para: MouseEvent) {
+  this.showError();
+
+  // 如果帳號或密碼為空，則顯示錯誤訊息
+  if (!(this.user.account && this.user.account)) {
+    alert('請輸入完整的帳號和密碼');
+    return;
+  } else {
+    alert('登入成功!');
   }
-
-    noUsername=false;
-    noPassword=false;
-
-showError() {
-    if (!this.username) {
-        this.noUsername = true;
-    } else  {
-        this.noUsername = false;
-    }
-    if (!this.password) {
-      this.noPassword = true;
-  } else  {
-    this.noPassword= false;
-  }
-
 }
 
-  }
+showError() {
+  // 檢查帳號是否為空
+  this.noUsername = this.user.account.trim() === "";
+
+  // 檢查密碼是否為空
+  this.noPassword = this.user.password.trim() === "";
+}
+
+}
